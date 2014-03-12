7b2e662956d4be5429fb29ed7c4dd36d1865d4be
huang.xinghui <huang.x.hui@gmail.com>
2014-03-12 21:18:20 +0800

    文件引入添加/

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
  * Copyright: Copyright 2013 ZTESOFT, Inc.
  */
 require.config({
-  'baseUrl': 'js/lib',
+  'baseUrl': '/js/lib',
   'paths': {
     'jquery': 'jquery-2.1.0.min'
   }
@@ -19,18 +19,10 @@ require(['jquery'], function($) {
 
       // 获取网页文档对象滚动条的垂直偏移
       var scrollTopNum = $(document).scrollTop(),
-      // 获取浏览器当前窗口的高度
-          winHeight = $(window).height(),
           returnTop = $("div.returnTop");
 
       // 滚动条的垂直偏移大于 0 时显示，反之隐藏
       (scrollTopNum > 0) ? returnTop.fadeIn("fast") : returnTop.fadeOut("fast");
-
-      // 给 IE6 定位
-      if (!-[1,]&&!window.XMLHttpRequest) {
-        returnTop.css("top", scrollTopNum + winHeight - 200);
-      }
-
     });
 
     // 点击按钮后，滚动条的垂直方向的值逐渐变为0，也就是滑动向上的效果
