4a4997c64a35a9c0b83cbce78488652d53d6129d
huang.xinghui <huang.x.hui@gmail.com>
2014-03-29 21:09:46 +0800

    Google分析添加

--- js/main.js
+++ js/main.js
@@ -22,3 +22,12 @@ require(['jquery', 'bootstrap'], function($) {
     $('.sidebar').css('min-height',$(document).height());
   });
 });
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-48905530-1', 'huang-x-h.github.io');
+ga('send', 'pageview');
